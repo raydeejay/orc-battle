229b6843b490df29024268114f2bc8136ceff4b9
Sergi Reyner <sergi.reyner@gmail.com>
2014-10-04 02:09:35 +0100

    Reworked the game as an SDL application
    Replaced sb-ext:exit with sdl:push-quit-event.
    
    None of the game functionality works yet since it's meant to work with
    text. This is basically me doing experiments :)

--- orc-battle.lisp
+++ orc-battle.lisp
@@ -8,11 +8,50 @@
 (defun randval (n)
   (1+ (random (max 1 n))))
 
+;; exported function to start the game
+(defun start ()
+  (setf *random-state* (make-random-state t))
+  (init-monsters)
+  (init-player)
+  (sdl:with-init ()
+    (sdl:window 640 480 :title-caption "Orc Battle")
+    (sdl:initialise-default-font sdl:*font-8x8*)
+    (sdl:draw-string-solid-* "hello hello" 10 10 :color sdl:*white*)
+    (sdl:update-display)
+    (sdl:with-events (:poll)
+      (:quit-event () t)
+      (:key-down-event (:key key)
+                       (when (or (sdl:key= key :sdl-key-escape)
+                                 (sdl:key= key :sdl-key-q))
+                         (sdl:push-quit-event))
+                       (when (sdl:key= key :sdl-key-a)
+                         (print-on-sdl "Pressed key A!"))
+
+                       )
+      (:video-expose-event () (sdl:update-display))
+
+      (game-loop)))
+  (fresh-line)
+  (end-game))
+
+(defun end-game ()
+  (when (player-dead)
+    (princa :red "You have been killed. Game Over." :reset))
+  (when (monsters-dead)
+    (princa :green :bold "Congratulations! You have vanquished all of your foes." :reset)
+    (fresh-line)))
+
+;; test sdl stuff
+(defun print-on-sdl (str)
+  (sdl:draw-string-solid-* str 10 440 :color sdl:*white*)
+  (sdl:update-display))
+
 ;; turn function
 (defun player-attack ()
   (fresh-line)
   (princa :cyan :bold "Attack style: [s]tab [d]ouble swing [r]oundhouse: " :reset)
   (finish-output nil)
+  (print-on-sdl "You are attacking now!")
   (let ((input (read-line)))
     (cond ((string-equal input "s")
            (monster-hit (pick-monster)
@@ -29,9 +68,7 @@
              (unless (monsters-dead)
                (monster-hit (random-monster) 1))))
           ((string-equal input "q")
-           (sb-ext:exit))
-          ;; (otherwise (read-char)
-          ;;            (player-attack))))
+           (sdl:push-quit-event))
           (t (player-attack)))))
 
 ;; main loop
@@ -47,21 +84,11 @@
          (lambda (m)
            (or (monster-dead m) (monster-attack m)))
          *monsters*)
-    (game-loop)))
+    (game-loop))
 
-;; exported function to start the game
-(defun start ()
-  (setf *random-state* (make-random-state t))
-  (init-monsters)
-  (init-player)
-  (game-loop)
-  (fresh-line)
-  (when (player-dead)
-    (princa :red "You have been killed. Game Over." :reset))
-  (when (monsters-dead)
-    (princa :green :bold "Congratulations! You have vanquished all of your foes." :reset))
-  (fresh-line)
-  (sb-ext:quit))
+  (when (or (player-dead) (monsters-dead))
+    (sdl:push-quit-event)))
+
 
 (defun main (argv)
   (declare (ignore argv))
