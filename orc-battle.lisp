0f421323a0bd626669a2fab20ed15a0b3e2da41e
Sergi Reyner <sergi.reyner@gmail.com>
2014-10-07 03:56:19 +0100

    More changes :D

--- orc-battle.lisp
+++ orc-battle.lisp
@@ -7,20 +7,25 @@
 (defparameter *console-width* 80)
 (defparameter *console-height* 40)
 
-(defparameter *input-mode* nil)
-
+(defparameter *game-mode* nil)
 (defparameter *input-modes* ())
+(defparameter *text-input* "")
 
 ;; a little helper function
 (defun randval (n)
   (1+ (random (max 1 n))))
 
+(defun init-game ()
+  (setf *random-state* (make-random-state t))
+  (setf *game-mode* #'player-attack)
+  (setf *input-modes* ())
+  (setf *text-input* "")
+  (push #'global-input-mode *input-modes*)
+  (push #'attack-input-mode *input-modes*))
+
 ;; exported function to start the game
 (defun start ()
-  (setf *random-state* (make-random-state t))
-  (setf *input-mode* 'attack)
-  (push #'attack-input-mode *input-modes*)
-  (push #'global-input-mode *input-modes*)
+  (init-game)
   (init-monsters)
   (init-player)
   (sdl:with-init ()
@@ -43,15 +48,53 @@
     (sdl:push-quit-event)))
 
 (defun attack-input-mode (key unicode)
-  (clear-rectangle 1 40 23 2)
+  (declare (ignorable unicode))
+  (clear-rectangle 1 40 23 3)
   (when (sdl:key= key :sdl-key-s)
     (print-on-sdl "Pressed key S!" :x 1 :y 40))
   (when (sdl:key= key :sdl-key-d)
     (print-on-sdl "Pressed key D!" :x 1 :y 40))
   (when (sdl:key= key :sdl-key-r)
     (print-on-sdl "Pressed key R!" :x 1 :y 40))
-  (print-on-sdl (format nil "From unicode: ~c ~d!" (code-char unicode) unicode)
-                :x 1 :y 41))
+  (when (member key '(:sdl-key-s :sdl-key-d :sdl-key-r))
+    (pop *input-modes*)
+    (push #'pickup-monster-input-mode *input-modes*))
+  ;;(print-on-sdl (format nil "From unicode: ~c ~d!" (code-char unicode) unicode)
+  ;;              :x 1 :y 41)
+  ) ; <-- careful
+
+(defun pickup-monster-input-mode (key unicode)
+  (clear-rectangle 1 42 23 3)
+  (when (and (sdl:key= key :sdl-key-backspace)
+             (> (length *text-input*) 0))
+    (setf *text-input* (subseq *text-input* 0 (1- (length *text-input*)))))
+  (when (<= (char-code #\0) unicode (char-code #\9))
+    (setf *text-input* (format nil "~a~c"
+                               *text-input*
+                               (code-char unicode))))
+  (print-on-sdl (format nil "Monster#: ~a" *text-input*)
+                :x 1 :y 42
+                :color sdl:*cyan*))
+
+(defun player-attack ()
+  (print-on-sdl "Attack style: [s]tab [d]ouble swing [r]oundhouse:"
+                :x 1 :y 30 :color sdl:*cyan*))
+
+(defun pick-monster ()
+  (princa :cyan :bold "Monster #: " :reset)
+  (finish-output nil)
+  (let ((x (parse-integer (read-line))))
+    (fresh-line)
+    (if (not (and (integerp x) (>= x 1) (<= x *monster-num*)))
+        (progn (princa "That is not a valid monster number.")
+               (fresh-line)
+               (pick-monster))
+        (let ((m (aref *monsters* (1- x))))
+          (if (monster-dead m)
+              (progn (princa "That monster is already dead.")
+                     (fresh-line)
+                     (pick-monster))
+              m)))))
 
 ;; game loop
 (defun game-loop ()
@@ -61,14 +104,12 @@
     (:key-down-event (:key key :unicode unicode)
                      (mapc (lambda (x)
                              (funcall x key unicode))
-                           *input-modes*)
-                     (princ unicode))
+                           *input-modes*))
     (:idle ()
            (show-player)
            (show-monsters)
-           (when (eq *input-mode* 'attack)
-             (print-on-sdl "Attack style: [s]tab [d]ouble swing [r]oundhouse:" :x 1 :y 30)
-             (sdl:update-display)))))
+           (funcall *game-mode*)
+           (sdl:update-display))))
 
 (defun end-game ()
   (when (player-dead)
